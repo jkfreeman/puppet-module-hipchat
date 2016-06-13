e148ef612b4ca8f20b20a6a192aa4bf81898771e
Josh Freeman <josh.freeman@gtri.gatech.edu>
2016-06-13 14:08:56 -0400

    Adds templates directory, and HipChat.ini.erb file. Adds code in init.pp to reference erb file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,26 @@
 class hipchat(
-  $manage_repo = true,
-) {
+    $manage_repo = true,
+    ) {
 
   if $manage_repo {
     require hipchat::repo
   }
-  
+
   package { 'hipchat':
     ensure => installed,
+  }
+  file { [ "/etc/skel/.config", "/etc/skel/.config/Atlassian" ]:
+    ensure  => "directory",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  file { '/etc/skel/.config/Atlassian/HipChat.ini' :
+    content => template('hipchat/HipChat.ini.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
   }
 
 }
